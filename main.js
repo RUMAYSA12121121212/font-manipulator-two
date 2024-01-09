a24eaa748c38ab91cc1207e1b13511c542e20310
RUMAYSA12121121212 <132350375+RUMAYSA12121121212@users.noreply.github.com>
2024-01-09 13:18:11 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+canvas = createCanvas(550,550);
+canvas.position(560,150);
+video = createCapture(VIDEO);
+video.size(550,500);
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on("pose", gotPoses);
+
+
+}
+function modelLoaded() {
+console.log("modelLoaded");
+}
+
+function gotPoses(results) {
+if (results.length > 0){
+    console.log(results);
+}
+}
+
+function preload(){
+
+}
+
+function draw(){
+background("blue");
+}
